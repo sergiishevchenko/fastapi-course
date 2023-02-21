60ad442d8544886284e425532db113c5fe1ebb79
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-02-21 10:09:14 +0100

    query params

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum
 
+
 app = FastAPI()
 
 
@@ -49,3 +50,35 @@ async def get_food(food_name: FoodEnum):
         return {"food_name": food_name, "message": "you are still healthy, but like sweet thing"}
 
     return {"food_name": food_name, "message": "I like chocolate milk"}
+
+
+fake_item_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_item_db[skip: skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def list_items(item_id: str, sample_query_param: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec."}
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec."}
+        )
+    return item
